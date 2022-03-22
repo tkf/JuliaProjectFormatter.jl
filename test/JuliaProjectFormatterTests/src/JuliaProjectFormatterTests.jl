dff32d27cdc442764d64b83ded6feb8864adbcd3
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-22 04:27:44 -0400

    Add more tests

--- test/JuliaProjectFormatterTests/src/JuliaProjectFormatterTests.jl
+++ test/JuliaProjectFormatterTests/src/JuliaProjectFormatterTests.jl
@@ -97,4 +97,71 @@ function test_subsubsub()
     end
 end
 
+function test_other_files()
+    bad = """
+    c = 3
+    b = 2
+    a = 1
+    """
+
+    good = """
+    a = 1
+    b = 2
+    c = 3
+    """
+
+    tree = [
+        # path => content
+        "Project.toml" => bad,
+        "sub/Project.toml" => bad,
+        "other_file.txt" => "other file with invalid TOML",
+        "sub/other_file.txt" => "other file with invalid TOML",
+    ]
+    withfiles(tree) do dir
+        format_projects(dir)
+        @test Text(read(joinpath(dir, "Project.toml"), String)) == Text(good)
+        @test Text(read(joinpath(dir, "sub/Project.toml"), String)) == Text(good)
+    end
+end
+
+function test_real_project()
+    bad = """
+    uuid = "139e0cf6-64b2-48ae-acd2-4285c0bb47c3"
+    authors = ["Takafumi Arakaki <aka.tkf@gmail.com> and contributors"]
+    version = "0.1.0-DEV"
+    name = "JuliaProjectFormatter"
+
+    [deps]
+    TOML = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+    [compat]
+    julia = "1.6"
+    """
+
+    good = """
+    name = "JuliaProjectFormatter"
+    uuid = "139e0cf6-64b2-48ae-acd2-4285c0bb47c3"
+    authors = ["Takafumi Arakaki <aka.tkf@gmail.com> and contributors"]
+    version = "0.1.0-DEV"
+
+    [deps]
+    TOML = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+    [compat]
+    julia = "1.6"
+    """
+
+    tree = [
+        # path => content
+        "Project.toml" => bad,
+        "sub/Project.toml" => bad,
+        "other_file.txt" => "other file with invalid TOML",
+        "sub/other_file.txt" => "other file with invalid TOML",
+    ]
+    withfiles(tree) do dir
+        format_projects(dir)
+        @test Text(read(joinpath(dir, "Project.toml"), String)) == Text(good)
+        @test Text(read(joinpath(dir, "sub/Project.toml"), String)) == Text(good)
+    end
+end
+
 end  # module JuliaProjectFormatterTests
